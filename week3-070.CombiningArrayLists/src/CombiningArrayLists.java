7dcc4ce63ca3fdcff99dbe4687df1117fa376537
eric-boone <therealeric@gmail.com>
2016-09-12 22:08:35 -0600

    up to 76 finished

--- week3-070.CombiningArrayLists/src/CombiningArrayLists.java
+++ week3-070.CombiningArrayLists/src/CombiningArrayLists.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class CombiningArrayLists {
+    public static void combine(ArrayList<Integer> first, ArrayList<Integer> second) {
+        for(int i : second) {
+            first.add(i);
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -23,7 +28,7 @@ public class CombiningArrayLists {
         // list2.add(7);
 
         // Implement the following method and then remove the comment
-        // combine(list1, list2);
+        combine(list1, list2);
         System.out.println(list1);
         System.out.println(list2);
 
---
+++ week4-073.Product/src/Product.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dad
+ */
+public class Product {
+    private String nameAtStart;
+    private double priceAtStart;
+    private int amountAtStart;
+    
+    public Product(String nameAtStart, double priceAtStart, int amountAtStart) {
+        this.nameAtStart = nameAtStart;
+        this.priceAtStart = priceAtStart;
+        this.amountAtStart = amountAtStart;
+    }
+    
+    public void printProduct() {
+        System.out.print(this.nameAtStart + 
+                ", price " +
+                this.priceAtStart +
+                ", amount " +
+                this.amountAtStart);
+    }
+}
