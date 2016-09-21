9900c018486665e01b060b0dbaf89ec530b68bb9
eric-boone <therealeric@gmail.com>
2016-09-20 22:47:59 -0600

    79 done

--- week4-079.NumberStatistics/src/Main.java
+++ week4-079.NumberStatistics/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
+         Scanner reader = new Scanner(System.in);
         // Write test code here
         // Remember to remove all the extra code when doing assignments  79.3 and 79.4
 
@@ -11,6 +11,32 @@ public class Main {
         // The second takes care of even numbers and the third the odd numbers.
 
         // The tests does not work if you do not create the objects in the correct order
-
+        
+         NumberStatistics sum = new NumberStatistics();
+         NumberStatistics even = new NumberStatistics();
+         NumberStatistics odd = new NumberStatistics();
+         
+         System.out.println("Type numbers:");
+         
+         
+         while(true) {
+             int input = Integer.parseInt(reader.nextLine());
+             
+             if(input == -1) {
+                 break;
+             }
+             
+             sum.addNumber(input);
+             
+             if(input % 2 == 1) {
+                 odd.addNumber(input);
+             } else {
+                 even.addNumber(input);
+             }
+         }
+         
+         System.out.println("sum: " + sum.sum());
+         System.out.println("sum of even: " + even.sum());
+         System.out.println("sum of odd: " + odd.sum());
     }
 }
