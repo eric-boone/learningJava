c650bce59a6cae81c8005261a2b3a937b64229b8
eric-boone <therealeric@gmail.com>
2016-09-28 23:02:48 -0600

    85 done

--- week5-085.Reformatory/src/Reformatory.java
+++ week5-085.Reformatory/src/Reformatory.java
@@ -1,8 +1,18 @@
 public class Reformatory {
+    private int weightCalledCounter;
 
     public int weight(Person person) {
         // return the weight of the person
-        return -1;
+        this.weightCalledCounter++;
+        return person.getWeight();
+    }
+    
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+    
+    public int totalWeightsMeasured() {
+        return this.weightCalledCounter;
     }
 
 }
