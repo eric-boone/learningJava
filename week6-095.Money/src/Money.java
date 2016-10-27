8e30c9f862ac2cc23a79ecd6ea469811cf257d12
eric-boone <therealeric@gmail.com>
2016-10-26 21:02:15 -0600

    95 done

--- week6-095.Money/src/Money.java
+++ week6-095.Money/src/Money.java
@@ -23,6 +23,33 @@ public class Money {
         return cents;
     }
 
+    public Money plus(Money added) {
+        return new Money(this.euros + added.euros, this.cents + added.cents);
+    }
+
+    public boolean less(Money compared) {
+        if (this.euros < compared.euros) {
+            return true;
+        } else if (this.euros == compared.euros && this.cents < compared.cents) {
+            return true;
+        }
+        return false;
+    }
+
+    public Money minus(Money decremented) {
+        if (this.less(decremented)) {
+            return new Money(0, 0);
+        } else if (this.cents - decremented.cents < 0) {
+            return new Money(this.euros - decremented.euros - 1,
+                    100 - decremented.cents);
+        } else if (this.cents < decremented.cents) {
+            return new Money(this.euros - decremented.euros - 1,
+                    this.cents - decremented.cents);
+        }
+        return new Money(this.euros - decremented.euros,
+                this.cents - decremented.cents);
+    }
+
     @Override
     public String toString() {
         String zero = "";
