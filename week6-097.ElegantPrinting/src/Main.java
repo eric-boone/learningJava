7e890b776b84b6bbb9197cc7437ba0e06a3d9475
eric-boone <therealeric@gmail.com>
2016-10-30 12:48:58 -0600

    87 done

--- week6-097.ElegantPrinting/src/Main.java
+++ week6-097.ElegantPrinting/src/Main.java
@@ -3,11 +3,16 @@ public class Main {
 
     public static void main(String[] args) {
         // test method here
-        int[] array = {5, 1, 3, 4, 2};
+        int[] array = {5, 1, 3, 4, 2, 7};
         printElegantly(array);
     }
 
     public static void printElegantly(int[] array) {
         // Write code here
+        int lastSpot = array[array.length - 1];
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println(lastSpot);
     }
 }
